9ec76b24a8bc11c303454bfe3b44704a83e0fc98
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-03-22 11:48:04 +0530

    Refactor rendering method in index.tsx

--- src/apps/frontend/index.tsx
+++ src/apps/frontend/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 
 import './satoshi.css';
 import './style.css';
-
 import App from './app.component';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  ReactDOM.render(<App />, document.getElementById('app'));
-});
+const container = document.getElementById('app');
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+ReactDOM.createRoot(container).render(<App />);
