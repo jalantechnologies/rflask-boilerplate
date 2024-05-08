dad52d005c10d7ab37b2214d9b53ac496b6c84bf
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-05-08 06:10:40 +0530

    removed service manager from test case

--- src/apps/backend/tests/modules/password_reset_token/base_test_password_reset_token.py
+++ src/apps/backend/tests/modules/password_reset_token/base_test_password_reset_token.py
@@ -4,14 +4,14 @@ import unittest
 from modules.account.internal.store.account_repository import AccountRepository
 from modules.config.config_manager import ConfigManager
 from modules.password_reset_token.internal.store.password_reset_token_repository import PasswordResetTokenRepository
-from modules.password_reset_token.password_reset_token_service_manager import PasswordResetTokenServiceManager
+from modules.password_reset_token.rest_api.password_reset_token_rest_api_server import PasswordResetTokenRestApiServer
 
 
 class BaseTestPasswordResetToken(unittest.TestCase):
     def setup_method(self, method: Callable) -> None:
         print(f"Executing:: {method.__name__}")
         ConfigManager.mount_config()
-        PasswordResetTokenServiceManager.create_rest_api_server()
+        PasswordResetTokenRestApiServer.create()
     
     def teardown_method(self, method: Callable) -> None:
         print(f"Executed:: {method.__name__}")
