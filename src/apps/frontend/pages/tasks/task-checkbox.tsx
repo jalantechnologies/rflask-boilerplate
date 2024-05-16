4f16104b494636d38d60c4a577411827fb9b28f6
Narendra jtc <narendra.singh@jalantechnologies.com>
2024-05-16 11:59:23 +0530

    refactor/ task-checkbox - converted block element to inline elems inside label

--- src/apps/frontend/pages/tasks/task-checkbox.tsx
+++ src/apps/frontend/pages/tasks/task-checkbox.tsx
@@ -9,13 +9,13 @@ const TaskCheckBox: React.FC<PropsWithChildren<TaskCheckboxProps>> = ({
   taskId,
 }) => (
   <label htmlFor={taskId} className="flex cursor-pointer select-none">
-    <div className="flex items-center content-center">
+    <span className="flex items-center content-center">
       <input type="checkbox" id={taskId} className="peer taskCheckbox sr-only" />
-      <div className="box flex mr-4 size-5 font-semibold items-center justify-center rounded border border-stroke ">
+      <span className="box flex mr-4 size-5 font-semibold items-center justify-center rounded border border-stroke ">
         <img alt="check" src="/assets/img/icon/form-checkbox-checkmark.svg" />
-      </div>
-    </div>
-    <p className="font-medium peer-checked:line-through">{children}</p>
+      </span>
+    </span>
+    <span className="font-medium peer-checked:line-through">{children}</span>
   </label>
 );
 
