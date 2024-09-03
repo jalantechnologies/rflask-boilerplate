00c3dc6f9bf8e405e0853d3781a42bd98e66a3ef
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-09-03 13:35:46 +0530

    testcases: add for otp verification

--- src/apps/backend/tests/modules/access_token/base_test_access_token.py
+++ src/apps/backend/tests/modules/access_token/base_test_access_token.py
@@ -1,6 +1,7 @@
 import unittest
 from typing import Callable
 
+from modules.otp.internal.store.otp_repository import OtpRepository
 from modules.access_token.rest_api.access_token_rest_api_server import AccessTokenRestApiServer
 from modules.account.internal.store.account_repository import AccountRepository
 
@@ -13,3 +14,4 @@ class BaseTestAccessToken(unittest.TestCase):
     def teardown_method(self, method: Callable) -> None:
         print(f"Executed:: {method.__name__}")
         AccountRepository.collection().delete_many({})
+        OtpRepository.collection().delete_many({})
