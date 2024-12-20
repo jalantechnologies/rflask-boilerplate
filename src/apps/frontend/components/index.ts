18cb217dd683440b6c1e9fea14a4eae8b173ffde
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-12-20 19:43:11 +0530

    feat: add Modal and additional typography components for enhanced UI

--- src/apps/frontend/components/index.ts
+++ src/apps/frontend/components/index.ts
@@ -7,10 +7,14 @@ import Input from './input';
 import PasswordInput from './input/password-input';
 import HorizontalStackLayout from './layouts/horizontal-stack-layout';
 import VerticalStackLayout from './layouts/vertical-stack-layout';
+import Modal from './modal';
 import OTP from './otp';
 import Select from './select';
 import H2 from './typography/h2';
+import HeadingLarge from './typography/heading-large';
+import LabelLarge from './typography/label-large';
 import ParagraphMedium from './typography/paragraph-medium';
+import ParagraphSmall from './typography/paragraph-small';
 
 export {
   Button,
@@ -19,10 +23,14 @@ export {
   FormControl,
   H2,
   Header,
+  HeadingLarge,
   HorizontalStackLayout,
   Input,
+  LabelLarge,
+  Modal,
   OTP,
   ParagraphMedium,
+  ParagraphSmall,
   PasswordInput,
   Select,
   VerticalStackLayout,
