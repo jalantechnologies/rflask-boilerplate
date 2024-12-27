c1c804764b81f565537ed98bb8c8167bcb6c5e38
Tarun <tarun.shetty@bettrsw.com>
2024-12-27 14:31:19 +0530

    refactor: Integrated config parser to read .ini
    removed unused settings and config_manager
    modified config_service to be the class that handles configuration
    modified all instances of config_manager to use config_service

--- src/apps/backend/modules/communication/sms_service.py
+++ src/apps/backend/modules/communication/sms_service.py
@@ -7,8 +7,7 @@ from modules.logger.logger import Logger
 class SMSService:
     @staticmethod
     def send_sms(*, params: SendSMSParams) -> None:
-        is_sms_enabled = ConfigService.get_bool("SMS_ENABLED")
-
+        is_sms_enabled = ConfigService.get_value(key="SMS_ENABLED",section="SMS") == "True"
         if not is_sms_enabled:
             Logger.warn(message=f"SMS is disabled. Could not send message - {params.message_body}")
             return
--- src/apps/backend/settings/production.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class ProductionSettings:
-    LOGGER_TRANSPORTS: tuple[str, str] = ("console", "papertrail")
-    SMS_ENABLED: bool = True
