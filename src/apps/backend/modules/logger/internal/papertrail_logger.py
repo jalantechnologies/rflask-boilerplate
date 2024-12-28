18706562eb47ca50780f64217ca7789d8bd08b6b
Tarun <tarun.shetty@bettrsw.com>
2024-12-28 15:19:51 +0530

    Fix: Preserve case for keys in .ini files when loading configurations
    - Overridden optionxform in ConfigParser to preserve the case of keys
    - Ensured environment variables are matched with original case from .ini files

--- src/apps/backend/modules/logger/internal/papertrail_logger.py
+++ src/apps/backend/modules/logger/internal/papertrail_logger.py
@@ -13,8 +13,8 @@ class PapertrailLogger(BaseLogger):
 
         # Create a console handler and set the level to INFO
         logger_config = PapertrailConfig(
-            host=ConfigService.get_value(key='HOST', section='PAPERTRAIL'),
-            port=int(ConfigService.get_value(key='PORT', section='PAPERTRAIL'))
+            host=ConfigService.get_value(key='PAPERTRAIL_HOST', section='PAPERTRAIL'),
+            port=int(ConfigService.get_value(key='PAPERTRAIL_PORT', section='PAPERTRAIL'))
         )
         papertrail_handler = SysLogHandler(address=(logger_config.host, logger_config.port))
         formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
