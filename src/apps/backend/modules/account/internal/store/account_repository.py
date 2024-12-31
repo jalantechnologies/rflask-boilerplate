c401260cf257a02cec4235f3b09cda0a0c3dc246
Tarun <tarun.shetty@bettrsw.com>
2024-12-31 10:10:36 +0530

    Feat: Added validation rules for mongodb collections

--- src/apps/backend/modules/account/internal/store/account_repository.py
+++ src/apps/backend/modules/account/internal/store/account_repository.py
@@ -1,5 +1,5 @@
 from pymongo.collection import Collection
-
+from pymongo.errors import OperationFailure
 from modules.account.internal.store.account_model import AccountModel
 from modules.application.repository import ApplicationRepository
 
@@ -10,4 +10,37 @@ class AccountRepository(ApplicationRepository):
     @classmethod
     def on_init_collection(cls, collection: Collection) -> bool:
         collection.create_index("username")
+        print("working")
+        validation_rules = {
+            "$jsonSchema": {
+                "bsonType": "object",
+                "required": ["username", "hashed_password"],
+                "properties": {
+                    "username": {"bsonType": "string", "description": "must be a string and is required"},
+                    "hashed_password": {"bsonType": "string", "description": "must be a string and is required"},
+                    "first_name": {"bsonType": "string"},
+                    "last_name": {"bsonType": "string"},
+                    "active": {"bsonType": "bool"},
+                    "phone_number": {
+                        "bsonType": "object",
+                        "properties": {"country_code": {"bsonType": "string"}, "phone_number": {"bsonType": "string"}},
+                        "description": "must be an object with country_code and phone_number",
+                    },
+                    "created_at": {"bsonType": "date"},
+                    "updated_at": {"bsonType": "date"},
+                },
+            }
+        }
+        try:
+            collection.database.command(
+                {"collMod": cls.collection_name, "validator": validation_rules, "validationLevel": "strict"}
+            )
+        except OperationFailure as e:
+            if "Collection does not exist" in str(e):
+                print(f"Collection '{cls.collection_name}' does not exist. Skipping schema validation.")
+            else:
+                print(f"OperationFailure occurred for collection '{cls.collection_name}': {e.details}")
+        except Exception as e:
+            print(f"An unexpected error occurred for collection '{cls.collection_name}': {e}")
+
         return True
--- src/apps/backend/modules/password_reset_token/internal/store/password_reset_token_repository.py
+++ src/apps/backend/modules/password_reset_token/internal/store/password_reset_token_repository.py
@@ -1,5 +1,5 @@
 from pymongo.collection import Collection
-
+from pymongo.errors import OperationFailure
 from modules.application.repository import ApplicationRepository
 from modules.password_reset_token.internal.store.password_reset_token_model import PasswordResetTokenModel
 
@@ -10,4 +10,30 @@ class PasswordResetTokenRepository(ApplicationRepository):
     @classmethod
     def on_init_collection(cls, collection: Collection) -> bool:
         collection.create_index("token")
+        validation_rules = {
+            "$jsonSchema": {
+                "bsonType": "object",
+                "required": ["account", "expires_at", "token", "is_used"],
+                "properties": {
+                    "account": {"bsonType": "objectId", "description": "must be an ObjectId and is required"},
+                    "expires_at": {"bsonType": "date", "description": "must be a valid date and is required"},
+                    "token": {"bsonType": "string", "description": "must be a string and is required"},
+                    "is_used": {"bsonType": "bool", "description": "must be a boolean and is required"},
+                    "_id": {"bsonType": "objectId", "description": "must be an ObjectId"},
+                },
+            }
+        }
+
+        try:
+            collection.database.command(
+                {"collMod": cls.collection_name, "validator": validation_rules, "validationLevel": "strict"}
+            )
+        except OperationFailure as e:
+            if "Collection does not exist" in str(e):
+                print(f"Collection '{cls.collection_name}' does not exist. Skipping schema validation.")
+            else:
+                print(f"OperationFailure occurred for collection '{cls.collection_name}': {e.details}")
+        except Exception as e:
+            print(f"An unexpected error occurred for collection '{cls.collection_name}': {e}")
+
         return True
