6eff8374e41666fcbe17c31287f8085a079fd34b
Tarun <tarun.shetty@bettrsw.com>
2025-01-02 14:38:28 +0530

    refactor: removed pydantic from password_reset_token_model

--- src/apps/backend/modules/password_reset_token/internal/store/password_reset_token_model.py
+++ src/apps/backend/modules/password_reset_token/internal/store/password_reset_token_model.py
@@ -1,22 +1,23 @@
 from datetime import datetime
-from typing import Annotated, Optional
-
+from typing import Optional
 from bson import ObjectId
-from pydantic import AfterValidator, BaseModel, ConfigDict, Field
-
 from modules.object_id.utils import object_id_validate
 
-PyObjectId = Annotated[ObjectId | str, AfterValidator(object_id_validate)]
-
-
-class PasswordResetTokenModel(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-    id: Optional[PyObjectId] = Field(None, alias="_id")
-    account: PyObjectId
-    expires_at: datetime
-    token: str
-    is_used: bool = False
+class PasswordResetTokenModel:
+    def __init__(
+        self,
+        id: Optional[ObjectId | str] = None,
+        account: Optional[ObjectId | str] = None,
+        expires_at: datetime = None,
+        token: str = None,
+        is_used: bool = False,
+        **kwargs
+    ):
+        self.id = object_id_validate(id) if id else kwargs.get("_id", None)
+        self.account = object_id_validate(account) if account else kwargs.get("account", None)
+        self.expires_at = expires_at
+        self.token = token
+        self.is_used = is_used
 
     @staticmethod
     def get_collection_name() -> str:
