4d433601d26223c15835a1bb6d4e3881e2daaead
Tarun <tarun.shetty@bettrsw.com>
2025-01-02 18:41:19 +0530

    fix: Added different get_type to config_service
    fix: improved file readablity

--- src/apps/backend/modules/access_token/access_token_service.py
+++ src/apps/backend/modules/access_token/access_token_service.py
@@ -40,8 +40,8 @@ class AccessTokenService:
 
     @staticmethod
     def __generate_access_token(*, account: Account) -> AccessToken:
-        jwt_signing_key = ConfigService.get_value(key="TOKEN_SIGNING_KEY", section="ACCOUNTS")
-        jwt_expiry = timedelta(days=int(ConfigService.get_value(key="TOKEN_EXPIRY_DAYS", section="ACCOUNTS")))
+        jwt_signing_key = ConfigService.get_string(key="TOKEN_SIGNING_KEY", section="ACCOUNTS")
+        jwt_expiry = timedelta(days=ConfigService.get_int(key="TOKEN_EXPIRY_DAYS", section="ACCOUNTS"))
         expiry_time = datetime.now() + jwt_expiry
         payload = {"account_id": account.id, "exp": (expiry_time).timestamp()}
         jwt_token = jwt.encode(payload, jwt_signing_key, algorithm="HS256")
@@ -52,7 +52,7 @@ class AccessTokenService:
     @staticmethod
     def verify_access_token(*, token: str) -> AccessTokenPayload:
 
-        jwt_signing_key = ConfigService.get_value(key="TOKEN_SIGNING_KEY", section="ACCOUNTS")
+        jwt_signing_key = ConfigService.get_string(key="TOKEN_SIGNING_KEY", section="ACCOUNTS")
 
         try:
             verified_token = jwt.decode(token, jwt_signing_key, algorithms=["HS256"])
