fe07d13ae34cb0eb978be7715c4078c3cd726683
Tarun <tarun.shetty@bettrsw.com>
2025-01-02 20:10:21 +0530

    fix: Added comments to lines with less readablity

--- src/apps/backend/modules/config/config_service.py
+++ src/apps/backend/modules/config/config_service.py
@@ -4,37 +4,41 @@ from modules.error.custom_errors import MissingKeyError
 from modules.common.types import ErrorCode
 from modules.common.config_utils import ConfigUtils
 
+
 class ConfigService:
     _config: configparser.ConfigParser = configparser.ConfigParser()
     config_path = ConfigUtils.get_parent_directory(__file__, 6) / "config"
 
     @staticmethod
-    def load_config()->None:
+    def load_config() -> None:
         config = configparser.ConfigParser()
-        config.optionxform = str # type: ignore
+        # ConfigParser converts all keys to lowercase by default.
+        # To preserve the case of keys, we set `config.optionxform = str`.
+        # The following line will always throw a lint error, hence the `# type: ignore`.
+        # Visit: https://github.com/python/mypy/issues/5062 for more information on the lint error.
+        config.optionxform = str  # type: ignore
         default_config = ConfigService.config_path / "default.ini"
-        app_env = os.environ.get('APP_ENV', "development")
+        app_env = os.environ.get("APP_ENV", "development")
         app_env_config = ConfigService.config_path / f"{app_env}.ini"
         config.read([default_config, app_env_config])
         ConfigService.__load_environment_variables(config=config)
         ConfigService._config = config
         config_dict = {
-            section: {
-                key: value
-                for key, value in ConfigService._config[section].items()
-            }
+            section: {key: value for key, value in ConfigService._config[section].items()}
             for section in ConfigService._config.sections()
         }
         print("config:", config_dict)
 
     @staticmethod
-    def __load_environment_variables(config: configparser.ConfigParser)->None:
+    def __load_environment_variables(config: configparser.ConfigParser) -> None:
+        """This function reads the custom_environment_file for keys
+        and retrieves associated values from the project environment"""
         custom_environment_file = ConfigService.config_path / "custom-environment-variables.ini"
         if not custom_environment_file.exists():
             return
-        
+
         env_config = configparser.ConfigParser()
-        env_config.optionxform = str # type: ignore
+        env_config.optionxform = str  # type: ignore
         env_config.read(custom_environment_file)
 
         for section in env_config.sections():
@@ -46,38 +50,36 @@ class ConfigService:
                     config[section][key] = env_var_value
                 elif not config.has_option(section, key):
                     config[section][key] = ""
-    
+
     @staticmethod
-    def get_string(*, key: str, section: str = 'DEFAULT') -> str:
+    def get_string(*, key: str, section: str = "DEFAULT") -> str:
         try:
             value = ConfigService._config.get(section, key)
-            return value 
+            return value
         except (configparser.NoOptionError, configparser.NoSectionError) as exc:
             raise MissingKeyError(missing_key=key, error_code=ErrorCode.MISSING_KEY) from exc
 
     @staticmethod
-    def get_int(*, key: str, section: str = 'DEFAULT') -> int:
+    def get_int(*, key: str, section: str = "DEFAULT") -> int:
         value = 0
         try:
-            value = ConfigService._config.getint(section,key)
+            value = ConfigService._config.getint(section, key)
         except ValueError as e:
             print(f"Error setting JWT expiry: {e}")
-        
+
         return value
-        
-        
 
     @staticmethod
-    def get_boolean(*, key: str, section: str = 'DEFAULT') -> bool:
+    def get_boolean(*, key: str, section: str = "DEFAULT") -> bool:
         value = False
         try:
-            value = ConfigService._config.getboolean(section,key)
+            value = ConfigService._config.getboolean(section, key)
         except ValueError as e:
             print(f"Error setting JWT expiry: {e}")
         return value
-    
+
     @staticmethod
-    def has_value(*, key: str, section: str = 'DEFAULT') -> bool:
+    def has_value(*, key: str, section: str = "DEFAULT") -> bool:
         if ConfigService._config.has_option(section, key):
             value = ConfigService._config.get(section, key)
             return bool(value)
