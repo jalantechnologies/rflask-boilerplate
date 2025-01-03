32b6de8d2cff25b6c1b8e3b7a813908c41cbe7f5
Tarun <tarun.shetty@bettrsw.com>
2025-01-03 16:03:47 +0530

    refactor: extracted model schema into a variable for better readability

--- src/apps/backend/modules/otp/internal/store/otp_repository.py
+++ src/apps/backend/modules/otp/internal/store/otp_repository.py
@@ -4,48 +4,49 @@ from modules.application.repository import ApplicationRepository
 from modules.otp.internal.store.otp_model import OtpModel
 from pymongo.errors import OperationFailure
 
+OTP_VALIDATION_SCHEMA = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": ["otp_code", "phone_number", "status", "active"],
+        "properties": {
+            "otp_code": {"bsonType": "string", "description": "must be a string and is required"},
+            "phone_number": {
+                "bsonType": "object",
+                "required": ["country_code", "phone_number"],
+                "properties": {
+                    "country_code": {"bsonType": "string", "description": "must be a string"},
+                    "phone_number": {"bsonType": "string", "description": "must be a string"},
+                },
+                "description": "must be an object with country_code and phone_number",
+            },
+            "status": {"bsonType": "string", "description": "must be a string and is required"},
+            "active": {"bsonType": "bool", "description": "must be a boolean and is required"},
+            "created_at": {"bsonType": "date", "description": "must be a valid date"},
+            "updated_at": {"bsonType": "date", "description": "must be a valid date"},
+            "_id": {"bsonType": "objectId", "description": "must be an ObjectId"},
+        },
+    }
+}
+
+
 class OtpRepository(ApplicationRepository):
     collection_name = OtpModel.get_collection_name()
 
     @classmethod
     def on_init_collection(cls, collection: Collection) -> bool:
-        
+
         collection.create_index("phone_number")
-
-        validation_rules = {
-            "$jsonSchema": {
-                "bsonType": "object",
-                "required": ["otp_code", "phone_number", "status", "active"],
-                "properties": {
-                    "otp_code": {"bsonType": "string", "description": "must be a string and is required"},
-                    "phone_number": {
-                        "bsonType": "object",
-                        "required": ["country_code", "phone_number"],
-                        "properties": {
-                            "country_code": {"bsonType": "string", "description": "must be a string"},
-                            "phone_number": {"bsonType": "string", "description": "must be a string"},
-                        },
-                        "description": "must be an object with country_code and phone_number",
-                    },
-                    "status": {"bsonType": "string", "description": "must be a string and is required"},
-                    "active": {"bsonType": "bool", "description": "must be a boolean and is required"},
-                    "created_at": {"bsonType": "date", "description": "must be a valid date"},
-                    "updated_at": {"bsonType": "date", "description": "must be a valid date"},
-                    "_id": {"bsonType": "objectId", "description": "must be an ObjectId"},
-                },
-            }
+        add_validation_command = {
+            "collMod": cls.collection_name, 
+            "validator": OTP_VALIDATION_SCHEMA, 
+            "validationLevel": "strict"
         }
-
         try:
-            collection.database.command(
-                {"collMod": cls.collection_name, "validator": validation_rules, "validationLevel": "strict"}
-            )
+            collection.database.command(add_validation_command)
         except OperationFailure as e:
+            # If the collection does not exist, create it with the validation rules
             if "Collection does not exist" in str(e):
-                print(f"Collection '{cls.collection_name}' does not exist. Skipping schema validation.")
+                collection.database.create_collection(cls.collection_name, validator=OTP_VALIDATION_SCHEMA)
             else:
-                print(f"OperationFailure occurred for collection '{cls.collection_name}': {e.details}")
-        except Exception as e:
-            print(f"An unexpected error occurred for collection '{cls.collection_name}': {e}")
-
+                print("OperationFailure occurred for collection accounts",e.details)
         return True
--- src/apps/backend/modules/password_reset_token/internal/store/password_reset_token_repository.py
+++ src/apps/backend/modules/password_reset_token/internal/store/password_reset_token_repository.py
@@ -3,6 +3,20 @@ from pymongo.errors import OperationFailure
 from modules.application.repository import ApplicationRepository
 from modules.password_reset_token.internal.store.password_reset_token_model import PasswordResetTokenModel
 
+PASSWORD_RESET_TOKEN_VALIDATION_SCHEMA = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": ["account", "expires_at", "token", "is_used"],
+        "properties": {
+            "account": {"bsonType": "objectId", "description": "must be an ObjectId and is required"},
+            "expires_at": {"bsonType": "date", "description": "must be a valid date and is required"},
+            "token": {"bsonType": "string", "description": "must be a string and is required"},
+            "is_used": {"bsonType": "bool", "description": "must be a boolean and is required"},
+            "_id": {"bsonType": "objectId", "description": "must be an ObjectId"},
+        },
+    }
+}
+
 
 class PasswordResetTokenRepository(ApplicationRepository):
     collection_name = PasswordResetTokenModel.get_collection_name()
@@ -10,30 +24,18 @@ class PasswordResetTokenRepository(ApplicationRepository):
     @classmethod
     def on_init_collection(cls, collection: Collection) -> bool:
         collection.create_index("token")
-        validation_rules = {
-            "$jsonSchema": {
-                "bsonType": "object",
-                "required": ["account", "expires_at", "token", "is_used"],
-                "properties": {
-                    "account": {"bsonType": "objectId", "description": "must be an ObjectId and is required"},
-                    "expires_at": {"bsonType": "date", "description": "must be a valid date and is required"},
-                    "token": {"bsonType": "string", "description": "must be a string and is required"},
-                    "is_used": {"bsonType": "bool", "description": "must be a boolean and is required"},
-                    "_id": {"bsonType": "objectId", "description": "must be an ObjectId"},
-                },
-            }
+        add_validation_command = {
+            "collMod": cls.collection_name, 
+            "validator": PASSWORD_RESET_TOKEN_VALIDATION_SCHEMA, 
+            "validationLevel": "strict"
         }
-
         try:
-            collection.database.command(
-                {"collMod": cls.collection_name, "validator": validation_rules, "validationLevel": "strict"}
-            )
+            collection.database.command(add_validation_command)
         except OperationFailure as e:
+            # If the collection does not exist, create it with the validation rules
             if "Collection does not exist" in str(e):
-                print(f"Collection '{cls.collection_name}' does not exist. Skipping schema validation.")
+                collection.database.create_collection(cls.collection_name, validator=PASSWORD_RESET_TOKEN_VALIDATION_SCHEMA)
             else:
-                print(f"OperationFailure occurred for collection '{cls.collection_name}': {e.details}")
-        except Exception as e:
-            print(f"An unexpected error occurred for collection '{cls.collection_name}': {e}")
-
+                print("OperationFailure occurred for collection accounts",e.details)
         return True
+
