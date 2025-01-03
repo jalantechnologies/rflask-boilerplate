d8c869ae3692afc2c2f9dcedbdcaac7d477fdfb7
Tarun <tarun.shetty@bettrsw.com>
2025-01-03 16:03:47 +0530

    refactor: extracted model schema into a variable for better readability

--- src/apps/backend/modules/account/internal/store/account_repository.py
+++ src/apps/backend/modules/account/internal/store/account_repository.py
@@ -3,6 +3,26 @@ from pymongo.errors import OperationFailure
 from modules.account.internal.store.account_model import AccountModel
 from modules.application.repository import ApplicationRepository
 
+ACCOUNT_VALIDATION_SCHEMA = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": ["username", "hashed_password"],
+        "properties": {
+            "username": {"bsonType": "string", "description": "must be a string and is required"},
+            "hashed_password": {"bsonType": "string", "description": "must be a string and is required"},
+            "first_name": {"bsonType": "string"},
+            "last_name": {"bsonType": "string"},
+            "active": {"bsonType": "bool"},
+            "phone_number": {
+                "bsonType": ["object", "null"],
+                "properties": {"country_code": {"bsonType": "string"}, "phone_number": {"bsonType": "string"}},
+                "description": "must be an object with country_code and phone_number",
+            },
+            "created_at": {"bsonType": "date"},
+            "updated_at": {"bsonType": "date"},
+        },
+    }
+}
 
 class AccountRepository(ApplicationRepository):
     collection_name = AccountModel.get_collection_name()
@@ -10,36 +30,19 @@ class AccountRepository(ApplicationRepository):
     @classmethod
     def on_init_collection(cls, collection: Collection) -> bool:
         collection.create_index("username")
-        validation_rules = {
-            "$jsonSchema": {
-                "bsonType": "object",
-                "required": ["username", "hashed_password"],
-                "properties": {
-                    "username": {"bsonType": "string", "description": "must be a string and is required"},
-                    "hashed_password": {"bsonType": "string", "description": "must be a string and is required"},
-                    "first_name": {"bsonType": "string"},
-                    "last_name": {"bsonType": "string"},
-                    "active": {"bsonType": "bool"},
-                    "phone_number": {
-                        "bsonType": ["object", "null"],
-                        "properties": {"country_code": {"bsonType": "string"}, "phone_number": {"bsonType": "string"}},
-                        "description": "must be an object with country_code and phone_number",
-                    },
-                    "created_at": {"bsonType": "date"},
-                    "updated_at": {"bsonType": "date"},
-                },
-            }
-        }
-        try:
-            collection.database.command(
-                {"collMod": cls.collection_name, "validator": validation_rules, "validationLevel": "strict"}
-            )
-        except OperationFailure as e:
-            if "Collection does not exist" in str(e):
-                print(f"Collection '{cls.collection_name}' does not exist. Skipping schema validation.")
-            else:
-                print(f"OperationFailure occurred for collection '{cls.collection_name}': {e.details}")
-        except Exception as e:
-            print(f"An unexpected error occurred for collection '{cls.collection_name}': {e}")
 
+        add_validation_command = {
+            "collMod": cls.collection_name, 
+            "validator": ACCOUNT_VALIDATION_SCHEMA, 
+            "validationLevel": "strict"
+        }
+
+        try:
+            collection.database.command(add_validation_command)
+        except OperationFailure as e:
+            # If the collection does not exist, create it with the validation rules
+            if "Collection does not exist" in str(e):
+                collection.database.create_collection(cls.collection_name, validator=ACCOUNT_VALIDATION_SCHEMA)
+            else:
+                print("OperationFailure occurred for collection accounts",e.details)
         return True
