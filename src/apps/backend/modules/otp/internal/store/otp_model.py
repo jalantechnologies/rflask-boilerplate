5754457f651f418360098fcea3a7677a24b6a85b
Tarun <tarun.shetty@bettrsw.com>
2025-01-07 20:27:32 +0530

    refactor: otp_model and password_reset_token_model into dataclass for improved readablity
    refactor: replaced phone_number.__dict__ with asdict(phone_number) for readablity purposes

--- src/apps/backend/modules/otp/internal/store/otp_model.py
+++ src/apps/backend/modules/otp/internal/store/otp_model.py
@@ -2,50 +2,39 @@ from datetime import datetime
 from typing import Any, Optional
 from bson import ObjectId
 from modules.account.types import PhoneNumber
-import json
+from dataclasses import dataclass,asdict
 
+@dataclass
 class OtpModel:
-    def __init__(
-        self,
-        id: Optional[ObjectId | str] = None,
-        active: bool = True,
-        otp_code: str = "",
-        phone_number: Optional[PhoneNumber|dict] = None,
-        status: str = "",
-        created_at: Optional[datetime] = None,
-        updated_at: Optional[datetime] = None,
-        **kwargs:Any
-    )->None:
-        # Use object_id_validate for the ID
-        self.id = id if id else kwargs.get("_id", None)
-        self.active = active
-        self.otp_code = otp_code
-        self.phone_number = PhoneNumber(**phone_number) if isinstance(phone_number, dict) else phone_number
-        self.status = status
-        self.created_at = created_at or datetime.now()
-        self.updated_at = updated_at or datetime.now()
-
-    def to_json(self) -> str:
-        return json.dumps(self.to_dict())
-
-    def to_dict(self) -> dict[str, Any]:
-        return {
-            "_id": str(self.id) if self.id else None,
-            "active": self.active,
-            "otp_code": self.otp_code,
-            "phone_number": self.phone_number.__dict__ if self.phone_number else None,
-            "status": self.status,
-            "created_at": self.created_at if self.created_at else None,
-            "updated_at": self.updated_at if self.updated_at else None,
-        }
+    id: Optional[ObjectId | str]
+    otp_code: str
+    phone_number: Optional[PhoneNumber]
+    status: str
+    active: bool
+    created_at: Optional[datetime] = datetime.now()
+    updated_at: Optional[datetime] = datetime.now()
 
     def to_bson(self) -> dict[str, Any]:
-        data = self.to_dict()
-        # Remove the _id if it's None
-        if "_id" in data and data["_id"] is None:
-            del data["_id"]
-
+        data = asdict(self)
+        if data.get("id") is not None:
+            data["_id"] = data.pop("id")
+        else:
+            data.pop("id", None)
         return data
+    
+    @classmethod
+    def from_bson(cls,bson_data:dict)->"OtpModel":
+        phone_number_data = bson_data.get("phone_number")
+        phone_number = PhoneNumber(**phone_number_data) if phone_number_data else None
+        return cls(
+            id=bson_data.get("_id"),
+            otp_code=bson_data.get("otp_code",""),
+            phone_number=phone_number,
+            status=bson_data.get("status",""),
+            active=bson_data.get("active",""),
+            created_at=bson_data.get("created_at"),
+            updated_at=bson_data.get("updated_at")
+        )
 
     @staticmethod
     def get_collection_name() -> str:
