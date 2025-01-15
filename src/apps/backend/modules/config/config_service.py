2da6b47931fa7aca16d7cc01003308650de769c7
Just-a-random-username <arjunagnihotri0211@gmail.com>
2025-01-15 11:42:35 +0530

    Removed all instances and references of papertrail

--- src/apps/backend/modules/config/config_service.py
+++ src/apps/backend/modules/config/config_service.py
@@ -1,6 +1,5 @@
 from modules.common.dict_util import DictUtil
 from modules.config.config_manager import ConfigManager
-from modules.config.types import PapertrailConfig
 from modules.config.types import DatadogConfig
 
 
@@ -22,20 +21,11 @@ class ConfigService:
         return DictUtil.required_get_tuple(input_dict=ConfigManager.config, key="LOGGER_TRANSPORTS")
 
     @staticmethod
-    def get_papertrail_config() -> PapertrailConfig:
-        return PapertrailConfig(
-            host=DictUtil.required_get_str(input_dict=ConfigManager.config, key="PAPERTRAIL_HOST"),
-            port=int(DictUtil.required_get_str(input_dict=ConfigManager.config, key="PAPERTRAIL_PORT")),
-        )
-
-    @staticmethod
     def get_datadog_config() -> "DatadogConfig":
         return DatadogConfig(
             api_key=DictUtil.required_get_str(input_dict=ConfigManager.config, key="DATADOG_API_KEY"),
             application_key=DictUtil.required_get_str(input_dict=ConfigManager.config, key="DATADOG_APPLICATION_KEY"),
         )
-
-
 
     @staticmethod
     def get_accounts_config() -> dict:
--- src/apps/backend/modules/logger/internal/datadog_logger.py
+++ src/apps/backend/modules/logger/internal/datadog_logger.py
@@ -4,6 +4,7 @@ from modules.logger.internal.base_logger import BaseLogger
 from modules.config.config_service import ConfigService
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.logs_api import LogsApi
+from datadog_api_client.v2.model.http_log import HTTPLog
 from datadog_api_client.v2.model.content_encoding import ContentEncoding
 
 
@@ -20,29 +21,34 @@ class DatadogLogger(BaseLogger):
     def critical(self, *, message: str) -> None:
         api_client = ApiClient(self.config)
         apiInstance = LogsApi(api_client)
+        body = HTTPLog(message)
         response = apiInstance.submit_log(message)
-        self.logger.critical(msg=message)
+        self.logger.critical(msg=body)
 
     def debug(self, *, message: str) -> None:
         api_client = ApiClient(self.config)
         apiInstance = LogsApi(api_client)
+        body = HTTPLog(message)
         response = apiInstance.submit_log(message)
-        self.logger.debug(msg=message)
+        self.logger.debug(msg=body)
 
     def error(self, *, message: str) -> None:
         api_client = ApiClient(self.config)
         apiInstance = LogsApi(api_client)
+        body = HTTPLog(message)
         response = apiInstance.submit_log(message)
-        self.logger.error(msg=message)
+        self.logger.error(msg=body)
 
     def info(self, *, message: str) -> None:
         api_client = ApiClient(self.config)
         apiInstance = LogsApi(api_client)
+        body = HTTPLog(message)
         response = apiInstance.submit_log(message)
-        self.logger.info(msg=message)
+        self.logger.info(msg=body)
 
     def warn(self, *, message: str) -> None:
         api_client = ApiClient(self.config)
         apiInstance = LogsApi(api_client)
+        body = HTTPLog(message)
         response = apiInstance.submit_log(message)
-        self.logger.warning(msg=message)
+        self.logger.warning(msg=body)
