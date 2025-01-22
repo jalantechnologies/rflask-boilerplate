24df7171cedd6bb1ff650d5b225a9b52d45a38d0
Tarun <tarun.shetty@bettrsw.com>
2025-01-22 11:54:35 +0530

    refactor: formated code for better readablity

--- src/apps/backend/modules/password_reset_token/internal/store/password_reset_token_model.py
+++ src/apps/backend/modules/password_reset_token/internal/store/password_reset_token_model.py
@@ -4,6 +4,7 @@ from bson import ObjectId
 from dataclasses import dataclass
 from modules.common.base_model import BaseModel
 
+
 @dataclass
 class PasswordResetTokenModel(BaseModel):
     id: Optional[ObjectId | str]
@@ -11,15 +12,15 @@ class PasswordResetTokenModel(BaseModel):
     token: str
     expires_at: datetime
     is_used: bool = False
-    
+
     @classmethod
     def from_bson(cls, bson_data: dict) -> "PasswordResetTokenModel":
         return cls(
-            id = bson_data.get("_id"),
-            account = bson_data.get("account",),
-            token = bson_data.get("token",""),
-            expires_at = bson_data.get("expires_at",""),
-            is_used = bson_data.get("is_used","")
+            id=bson_data.get("_id"),
+            account=bson_data.get("account"),
+            token=bson_data.get("token", ""),
+            expires_at=bson_data.get("expires_at", ""),
+            is_used=bson_data.get("is_used", ""),
         )
 
     @staticmethod
