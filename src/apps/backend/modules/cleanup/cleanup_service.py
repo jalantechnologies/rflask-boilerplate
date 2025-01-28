42e3d2179cc14153b99c63b7b77153d9ebcea895
Sayan Bhattacharyya <sayan.bhattacharyya@bettrsw.com>
2025-01-28 16:39:12 +0530

    docs: update docstring for the cleanup `register` decorator

--- src/apps/backend/modules/cleanup/cleanup_service.py
+++ src/apps/backend/modules/cleanup/cleanup_service.py
@@ -9,6 +9,16 @@ class CleanupService:
     def register(main: bool = False) -> Callable:
         """
         Returns a decorator for registering cleanup hooks.
+
+        Example: For registering a cleanup hook for any module, use the following decorator:
+
+        @CleanupService.register()
+        def modulename_cleanup(params: SearchAccountByIdParams):
+            pass
+
+        The main hook is reserved for the final account deletion, hence, no other module should pass
+        the `main` parameter as `True`.
+
         :param main: If True, registers the hook as the main hook.
         """
 
@@ -22,6 +32,9 @@ class CleanupService:
     def check() -> Callable:
         """
         Returns a decorator for registering a pre-hook check.
+
+        This hook is reserved for checking if the account can be deleted. Hence, no other module
+        than `account` should use this.
         """
 
         def decorator(func: Callable) -> Callable:
