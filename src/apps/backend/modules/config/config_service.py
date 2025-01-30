ce547aa0e8e02eea69937923b774b57b8ff9d981
Tarun <tarun.shetty@bettrsw.com>
2025-01-30 09:49:19 +0530

    refactor: added Genric call to get_value

--- src/apps/backend/modules/config/config_service.py
+++ src/apps/backend/modules/config/config_service.py
@@ -1,22 +1,21 @@
-from typing import TypeVar
+from typing import Generic,TypeVar
 from modules.common.types import ErrorCode
 from modules.error.custom_errors import MissingKeyError
 from modules.config.config import Config
 
 T = TypeVar('T')
 
-class ConfigService:
-
+class ConfigService(Generic[T]):
     @staticmethod
     def load_config() -> None:
         Config.load_config()
 
-    @staticmethod
-    def get_value(key: str) -> T: # type: ignore
+    @classmethod
+    def get_value(cls,key: str) -> T:
         value = Config.get(key)
         if value is None:
             raise MissingKeyError(missing_key=key, error_code=ErrorCode.MISSING_KEY)
-        return value # type: ignore
+        return value  # type: ignore
 
     @staticmethod
     def has_value(key: str) -> bool:
