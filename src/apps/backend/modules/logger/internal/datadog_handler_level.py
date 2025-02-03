bd10b0de24d9d5556924581c2725f440754d9e67
Just-a-random-username <arjunagnihotri0211@gmail.com>
2025-02-04 01:09:01 +0530

    Implemented logger_enum file to store log levels as enum, and changed datadog_handler_level to use the enum values for the logger level

--- src/apps/backend/modules/logger/internal/datadog_handler_level.py
+++ src/apps/backend/modules/logger/internal/datadog_handler_level.py
@@ -1,24 +1,13 @@
 from modules.config.config_service import ConfigService
+from modules.logger.internal.logger_enum import Levels
 import logging
 
 class LogLevel:
     @staticmethod
     def get_level() -> int:
         ddconfig = ConfigService.get_datadog_config()
-        level = ddconfig.datadog_log_level.lower()
-        logging_levels = ["notset","debug","info","warning","error","critical"]
-        if level.lower() not in logging_levels:
-            return logging.DEBUG
-        else :
-            if level == logging_levels[0]:
-                return logging.NOTSET
-            elif level == logging_levels[1]:
-                return logging.DEBUG
-            elif level == logging_levels[2]:
-                return logging.INFO
-            elif level == logging_levels[3]:
-                return logging.WARNING
-            elif level == logging_levels[4]:
-                return logging.ERROR
-            else :
-                return logging.CRITICAL
+        datadog_level = ddconfig.datadog_log_level.lower()
+        for level in Levels:
+            if datadog_level.lower()==level[0]:
+                return level[1]
+        return logging.DEBUG
