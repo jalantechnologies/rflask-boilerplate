7da7eed56b96a636d810df3ea8b3477ba3c46eff
suren <v.suren.dran280@gmail.com>
2025-02-04 19:24:33 +0530

    Improve form responsiveness
    Adjusted form height to be dynamic instead of extending to full screen.
    
    - Used `min-h-[70vh] md:min-h-[50vh]` to prevent excessive height
    
    These changes enhance UI consistency and responsiveness across screen sizes.

--- src/apps/frontend/pages/authentication/authentication-form-layout.tsx
+++ src/apps/frontend/pages/authentication/authentication-form-layout.tsx
@@ -12,7 +12,7 @@ const AuthenticationFormLayout: React.FC<AuthenticationFormLayoutProps> = ({
   layoutType = 'background-image', // The prompt code for the layout (e.g., "half-image", "full-form","background-image")
 }) => (
   <CustomLayout layoutType={layoutType}>
-    <div className="flex min-h-screen flex-wrap items-start justify-center p-4 md:p-6 2xl:p-10">
+    <div className="flex h-auto min-h-[70vh] items-center justify-center p-4 md:min-h-[50vh] lg:min-h-[60vh]">
       <div className="w-full rounded-sm border border-stroke bg-white p-4 shadow-default dark:border-strokedark dark:bg-boxdark sm:p-4 md:w-full xl:w-full">
         {children}
       </div>
