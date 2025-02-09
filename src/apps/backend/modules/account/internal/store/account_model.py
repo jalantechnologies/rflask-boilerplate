6885530ef8c4e866a115c7fc4d64262df2bed973
Killg0d <shettytarun2002@example.com>
2025-02-09 20:22:21 +0530

    refactor: model variables alphabetically

--- src/apps/backend/modules/account/internal/store/account_model.py
+++ src/apps/backend/modules/account/internal/store/account_model.py
@@ -8,12 +8,14 @@ from modules.common.base_model import BaseModel
 
 @dataclass
 class AccountModel(BaseModel):
-    id: Optional[ObjectId | str]
+    
     first_name: str
     hashed_password: str
-    phone_number: Optional[PhoneNumber]
+    id: Optional[ObjectId | str]
     last_name: str
+    phone_number: Optional[PhoneNumber]
     username: str
+    
     active: bool = True
     created_at: Optional[datetime] = datetime.now()
     updated_at: Optional[datetime] = datetime.now()
@@ -23,12 +25,12 @@ class AccountModel(BaseModel):
         phone_number_data = bson_data.get("phone_number")
         phone_number = PhoneNumber(**phone_number_data) if phone_number_data else None
         return cls(
-            id=bson_data.get("_id"),
             active=bson_data.get("active", True),
             first_name=bson_data.get("first_name", ""),
             hashed_password=bson_data.get("hashed_password", ""),
-            phone_number=phone_number,
+            id=bson_data.get("_id"),
             last_name=bson_data.get("last_name", ""),
+            phone_number=phone_number,
             username=bson_data.get("username", ""),
             created_at=bson_data.get("created_at"),
             updated_at=bson_data.get("updated_at"),
