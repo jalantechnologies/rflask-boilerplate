f0077ad1bba1a11dfe5bbcb4b23a91ea47e991f8
Killg0d <shettytarun2002@example.com>
2025-02-10 15:12:20 +0530

    refactor: split ConfigFiles class into sub-classes for easier readablity

---
+++ src/apps/backend/modules/config/internals/default_config.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+from modules.config.internals.config_utils import ConfigUtil
+
+class DefaultConfig:
+    
+    filename:str = "default.yml"
+    
+    @staticmethod
+    def load() -> dict[str,Any]:
+        return ConfigUtil.read_yml_from_config_dir(DefaultConfig.filename)
