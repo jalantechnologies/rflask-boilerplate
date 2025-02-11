6bcc20fbca135a67f9d71d54a9ab86a4b045dc5c
Killg0d <shettytarun2002@example.com>
2025-02-11 08:33:02 +0530

    refactor: fixed nits like space, comments

--- src/apps/backend/modules/config/internals/config_utils.py
+++ src/apps/backend/modules/config/internals/config_utils.py
@@ -6,7 +6,7 @@ import yaml
 
 
 class ConfigUtil:
-        
+
     @staticmethod
     def deep_merge(dict1: dict[str, Any], dict2: dict[str, Any]) -> dict[str, Any]:
         for key, value in dict2.items():
@@ -15,22 +15,24 @@ class ConfigUtil:
             else:
                 dict1[key] = value
         return dict1
-    
+
     @staticmethod
-    def read_yml_from_config_dir(filename:str) -> dict[str, Any]:
+    def read_yml_from_config_dir(filename: str) -> dict[str, Any]:
         config_path = ConfigUtil._get_base_config_directory(__file__)
-        file_path = config_path / filename # Path lib's join operator
+        file_path = config_path / filename  # Path lib's join operator
         try:
             with open(file_path, "r", encoding="utf-8") as file:
                 content = yaml.safe_load(file) or {}
         except FileNotFoundError:
+            # Raised when filename is not found in config dir
             raise FileNotFoundError(f"Config file '{filename}' not found in {config_path}")
         return content
-    
+
     @staticmethod
     def _get_base_config_directory(current_file: str) -> Path:
         starting_index = current_file.find("app")
         if starting_index == -1:
+            # Raised when 'app' is not in the project_path
             raise ValueError(f"'app' not found in current_file path: {current_file}")
 
         base_directory = current_file[: starting_index + len("app")]
@@ -38,7 +40,7 @@ class ConfigUtil:
         config_path = Path(config_directory)
 
         if not config_path.exists() or not config_path.is_dir():
+            # Raised when config dir is not created
             raise FileNotFoundError(f"Config directory does not exist: {config_directory}")
 
         return config_path
-    
