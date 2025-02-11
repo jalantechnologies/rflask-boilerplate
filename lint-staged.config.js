341ea005a035697c7e6e456e4402c66d3e2dd0df
Just-a-random-username <arjunagnihotri0211@gmail.com>
2025-02-12 05:14:00 +0530

    Revert "Added error-boundaries to automatically send logs to frontend. Created frontend_logger.py to send error logs recieved from frontend to datadog. Updated outdated hash for autoflake. Reinstalled autoflake due to getting error autoflake not found. Updating lint file to execute commands in proper shell"
    This reverts commit 6603e17cdcb8fbddb240374c461575935eb77d80.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   //'*?(x)': () => 'npm run fmt',
   '*.{js,ts,tsx}': ['prettier --write --ignore-path .prettierignore'],
-  '*.py': [
-    'sh -c pipenv run autoflake -i',
-    'pipenv run isort',
-    'pipenv run black',
-  ],
+  '*.py': ['pipenv run autoflake -i', 'pipenv run isort', 'pipenv run black'],
 };
