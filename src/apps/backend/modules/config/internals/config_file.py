c9827fe8d3812a82beb2d7dcbd11a10a703f27f0
Killg0d <shettytarun2002@example.com>
2025-02-11 08:33:02 +0530

    refactor: fixed nits like space, comments

--- src/apps/backend/modules/config/internals/config_file.py
+++ src/apps/backend/modules/config/internals/config_file.py
@@ -7,23 +7,23 @@ from modules.config.internals.app_env_config import AppEnvConfig
 from modules.config.internals.custom_env_config import CustomEnvConfig
 from modules.config.internals.config_utils import ConfigUtil
 
+
 class ConfigFiles:
+    content: dict[str, Any] = {}
 
-    def __init__(self) -> None:
-        self.content: dict[str, Any] = {}
+    @staticmethod
+    def load() -> None:
+        ConfigFiles.content = ConfigFiles._merge_configs()
 
-    def load(self) -> None:
-        self.content = self._merge_configs()
-        
-    def get_content(self) -> dict[str, Any]:
-        self.load()
-        return self.content
-    
-    def _merge_configs(self) -> dict[str, Any]:
+    @staticmethod
+    def get_config_contents() -> dict[str, Any]:
+        return ConfigFiles.content
+
+    @staticmethod
+    def _merge_configs() -> dict[str, Any]:
         default_content = DefaultConfig.load()
         app_env_content = AppEnvConfig.load()
         os_env_content = CustomEnvConfig.load()
 
         merged_content = ConfigUtil.deep_merge(default_content, app_env_content)
         return ConfigUtil.deep_merge(merged_content, os_env_content)
-        
--- src/apps/backend/modules/config/internals/default_config.py
+++ src/apps/backend/modules/config/internals/default_config.py
@@ -2,10 +2,11 @@ from typing import Any
 
 from modules.config.internals.config_utils import ConfigUtil
 
+
 class DefaultConfig:
-    
-    filename:str = "default.yml"
-    
+
+    filename: str = "default.yml"
+
     @staticmethod
-    def load() -> dict[str,Any]:
+    def load() -> dict[str, Any]:
         return ConfigUtil.read_yml_from_config_dir(DefaultConfig.filename)
