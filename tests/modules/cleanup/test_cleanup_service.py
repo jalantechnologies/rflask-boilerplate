69a920dd370895c6c5f51b1b49e6dd91624a131f
Sayan Bhattacharyya <sayan.bhattacharyya@bettrsw.com>
2025-02-20 11:32:15 +0530

    fix: resolve test issues

--- tests/modules/cleanup/test_cleanup_service.py
+++ tests/modules/cleanup/test_cleanup_service.py
@@ -1,8 +1,9 @@
-from unittest.mock import MagicMock, patch
-
-from tests.modules.cleanup.base_test_cleanup import BaseTestCleanup
+from unittest.mock import ANY, MagicMock, patch
 
 from modules.cleanup.cleanup_service import CleanupService
+from modules.cleanup.types import CreateAccountDeletionRequestParams
+
+from tests.modules.cleanup.base_test_cleanup import BaseTestCleanup
 
 
 class TestCleanupService(BaseTestCleanup):
@@ -27,8 +28,33 @@ class TestCleanupService(BaseTestCleanup):
         mock_push_hooks.assert_called_once()
 
     @patch(
+        "modules.account.account_service.AccountService.get_account_by_id",
+        return_value=True,
+    )
+    @patch("modules.account.account_service.AccountService.deactivate_account")
+    @patch(
         "modules.cleanup.internal.cleanup_manager.CleanupManager.queue_account_deletion"
     )
-    def test_queue_account_deletion(self, mock_queue_account_deletion):
+    def test_queue_account_deletion(
+        self, mock_queue_account_deletion, mock_deactivate_account, mock_get_account
+    ):
         CleanupService.queue_account_deletion(params=self.params)
-        mock_queue_account_deletion.assert_called_once_with(params=self.params)
+
+        mock_get_account.assert_called_once_with(params=self.params)
+        mock_deactivate_account.assert_called_once_with(params=self.params)
+        mock_queue_account_deletion.assert_called_once_with(
+            params=CreateAccountDeletionRequestParams(
+                account_id=self.params.id, requested_at=ANY
+            )
+        )
+
+    @patch(
+        "modules.account.account_service.AccountService.get_account_by_id",
+        return_value=None,
+    )
+    def test_queue_account_deletion_fails_if_account_not_found(self, mock_get_account):
+        with self.assertRaises(Exception) as context:
+            CleanupService.queue_account_deletion(params=self.params)
+
+        self.assertTrue("Account not found" in str(context.exception))
+        mock_get_account.assert_called_once_with(params=self.params)
