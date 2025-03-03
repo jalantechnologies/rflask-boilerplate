62ddc38f88cc6c5b7522886e8d50d67e33e7140a
Sayan Bhattacharyya <sayan.bhattacharyya@bettrsw.com>
2025-03-03 11:16:13 +0530

    refactor: remove rest api for workflows

--- src/apps/backend/server.py
+++ src/apps/backend/server.py
@@ -5,13 +5,17 @@ from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from bin.blueprints import api_blueprint, img_assets_blueprint, react_blueprint
-from modules.access_token.rest_api.access_token_rest_api_server import AccessTokenRestApiServer
+from modules.access_token.rest_api.access_token_rest_api_server import (
+    AccessTokenRestApiServer,
+)
 from modules.account.rest_api.account_rest_api_server import AccountRestApiServer
+from modules.config.config_manager import ConfigManager
 from modules.config.config_service import ConfigService
 from modules.error.custom_errors import AppError
 from modules.logger.logger_manager import LoggerManager
-from modules.password_reset_token.rest_api.password_reset_token_rest_api_server import PasswordResetTokenRestApiServer
-from modules.workflow.rest_api.workflow_rest_api_server import WorkflowRestApiServer
+from modules.password_reset_token.rest_api.password_reset_token_rest_api_server import (
+    PasswordResetTokenRestApiServer,
+)
 
 load_dotenv()
 
@@ -19,12 +23,13 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 # Mount deps
+ConfigManager.mount_config()
 LoggerManager.mount_logger()
 
 # Apply ProxyFix to interpret `X-Forwarded` headers if enabled in configuration
 # Visit: https://flask.palletsprojects.com/en/stable/deploying/proxy_fix/ for more information
-if ConfigService.has_value("is_server_running_behind_proxy") and ConfigService[bool].get_value(
-    "is_server_running_behind_proxy"
+if ConfigService.has_key("IS_SERVER_RUNNING_BEHIND_PROXY") and ConfigService.get_bool(
+    "IS_SERVER_RUNNING_BEHIND_PROXY"
 ):
     app.wsgi_app = ProxyFix(app.wsgi_app)  # type: ignore
 
@@ -35,10 +40,6 @@ api_blueprint.register_blueprint(access_token_blueprint)
 # Register password reset token apis
 password_reset_token_blueprint = PasswordResetTokenRestApiServer.create()
 api_blueprint.register_blueprint(password_reset_token_blueprint)
-
-# Register workflow apis
-workflow_blueprint = WorkflowRestApiServer.create()
-api_blueprint.register_blueprint(workflow_blueprint)
 
 # Register accounts apis
 account_blueprint = AccountRestApiServer.create()
--- tests/modules/workflow/base_test_workflow.py
+++ tests/modules/workflow/base_test_workflow.py
@@ -3,7 +3,6 @@ from typing import Callable
 
 from modules.config.config_manager import ConfigManager
 from modules.logger.logger_manager import LoggerManager
-from modules.workflow.rest_api.workflow_rest_api_server import WorkflowRestApiServer
 
 
 class BaseTestWorkflow(unittest.TestCase):
@@ -11,7 +10,6 @@ class BaseTestWorkflow(unittest.TestCase):
         print(f"Executing:: {method.__name__}")
         ConfigManager.mount_config()
         LoggerManager.mount_logger()
-        WorkflowRestApiServer.create()
 
     def teardown_method(self, method: Callable) -> None:
         print(f"Executed:: {method.__name__}")
