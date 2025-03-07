b370f55704b0bbd7e98915eebe4ff855fce60525
Just-a-random-username <arjunagnihotri0211@gmail.com>
2025-03-08 02:33:17 +0530

    Made changes to allow datadog to properly log events to datadog based on the level, instead of listing all logs as INFO. Also fixed a critical typo in loggers.py

--- src/apps/backend/modules/logger/internal/datadog_logger.py
+++ src/apps/backend/modules/logger/internal/datadog_logger.py
@@ -1,4 +1,5 @@
 import logging
+
 from modules.logger.internal.base_logger import BaseLogger
 from modules.logger.internal.datadog_handler import DatadogHandler
 from modules.logger.internal.datadog_handler_level import LogLevel
@@ -9,26 +10,29 @@ class DatadogLogger(BaseLogger):
         self.level = LogLevel.get_level()
         self.logger = logging.getLogger(__name__)
         self.format = "[%(asctime)s] - %(name)s - %(levelname)s - %(message)s"
-        self.formatter = logging.Formatter(
-            self.format,
-        )
+        self.formatter = logging.Formatter(self.format)
         self.logger.setLevel(LogLevel.get_level())
-        self.handler = DatadogHandler('flask')
+        self.handler = DatadogHandler("flask")
         self.handler.setLevel(LogLevel.get_level())
         self.handler.setFormatter(self.formatter)
         self.logger.addHandler(self.handler)
 
     def critical(self, *, message: str) -> None:
+        self.handler.setLevel(logging.CRITICAL)
         self.logger.critical(message)
 
     def debug(self, *, message: str) -> None:
+        self.handler.setLevel(logging.DEBUG)
         self.logger.debug(message)
 
     def error(self, *, message: str) -> None:
+        self.handler.setLevel(logging.ERROR)
         self.logger.error(message)
 
     def info(self, *, message: str) -> None:
+        self.handler.setLevel(logging.INFO)
         self.logger.info(message)
 
     def warn(self, *, message: str) -> None:
+        self.handler.setLevel(logging.WARNING)
         self.logger.warning(message)
