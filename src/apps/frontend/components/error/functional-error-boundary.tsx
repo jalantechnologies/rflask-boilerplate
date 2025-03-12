ff59badef0a93d42bbf0954940cc3c569dc1759b
Just-a-random-username <arjunagnihotri0211@gmail.com>
2025-03-12 14:34:10 +0530

    Made changes to satisfy eslint

--- src/apps/frontend/components/error/functional-error-boundary.tsx
+++ src/apps/frontend/components/error/functional-error-boundary.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import axios from 'axios';
+import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+
 import { JsonObject } from '../../types/common-types';
 
 interface ErrorData extends JsonObject {
@@ -12,7 +13,7 @@ interface ErrorData extends JsonObject {
 const logErrorToServer = async (
   error: Error,
   errorInfo: { componentStack: string },
-) => {
+): Promise<void> => {
   const errorData: ErrorData = {
     'error-name': error.name,
     'error-message': error.message,
@@ -33,11 +34,11 @@ const ErrorFallback = ({
   error: Error;
   resetErrorBoundary: () => void;
 }) => (
-  <div className="p-4 border border-red-500 bg-red-100 text-red-800 rounded">
-    <h2 className="font-bold text-lg">Something went wrong</h2>
+  <div className="rounded border border-red-500 bg-red-100 p-4 text-red-800">
+    <h2 className="text-lg font-bold">Something went wrong</h2>
     <p>{error.message}</p>
     <button
-      className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+      className="mt-2 rounded bg-blue-500 px-4 py-2 text-white"
       onClick={resetErrorBoundary}
     >
       Try Again
@@ -47,12 +48,10 @@ const ErrorFallback = ({
 
 const FunctionalErrorBoundary: React.FC<{ children: React.ReactNode }> = ({
   children,
-}) => {
-  return (
-    <ErrorBoundary FallbackComponent={ErrorFallback} onError={logErrorToServer}>
-      {children}
-    </ErrorBoundary>
-  );
-};
+}) => (
+  <ErrorBoundary FallbackComponent={ErrorFallback} onError={logErrorToServer}>
+    {children}
+  </ErrorBoundary>
+);
 
 export default FunctionalErrorBoundary;
