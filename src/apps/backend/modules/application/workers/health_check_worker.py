9ced7caaa8d548daa5abfc549cec576985029f17
Govind Sharma <govindgovind852@gmail.com>
2025-05-07 22:39:02 +0530

    refactor: change the worker architecture

--- src/apps/backend/modules/application/workers/health_check_worker.py
+++ src/apps/backend/modules/application/workers/health_check_worker.py
@@ -7,10 +7,13 @@ from modules.logger.logger import Logger
 
 
 class HealthCheckWorker(BaseWorker):
+    max_execution_time_in_seconds = 10
+    max_retries = 1
+
     @staticmethod
     async def execute(*args: Any) -> None:
         try:
-            res = requests.get("http://localhost:8080/api/")
+            res = requests.get("http://localhost:8080/api/", timeout=3)
 
             if res.status_code == 200:
                 Logger.info(message="Backend is healthy")
