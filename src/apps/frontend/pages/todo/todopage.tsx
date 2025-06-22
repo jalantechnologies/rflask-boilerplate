07290dfca171bdcb23c3f29367ade2ee376ad5fb
shivamj-0303 <shivam.jangid@bettrhq.com>
2025-06-22 22:36:14 +0530

    refactor: structure TodoPage with separate content component and context provider

--- src/apps/frontend/pages/todo/todopage.tsx
+++ src/apps/frontend/pages/todo/todopage.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { TodoProvider } from '../../contexts/todocontext';
 import TodoForm from '../../components/todo/todoform';
 import TodoList from '../../components/todo/todolist';
 import { useTodo } from '../../contexts/todocontext';
 
-const TodoPage = () => {
+const TodoContent: React.FC = () => {
   const { refresh } = useTodo();
 
   return (
     <div className="max-w-2xl mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">TODO</h1>
+      <h1 className="text-xl font-bold mb-4">Your TODOs</h1>
       <TodoForm onCreate={refresh} />
       <TodoList />
     </div>
   );
 };
+
+const TodoPage: React.FC = () => (
+  <TodoProvider>
+    <TodoContent />
+  </TodoProvider>
+);
 
 export default TodoPage;
