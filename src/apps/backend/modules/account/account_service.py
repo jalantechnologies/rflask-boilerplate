ae8f6fb133beb1dde0d748e02736d319b03250e7
Sarthak Gaikwad <sarthak.gaikwad@bettrhq.com>
2025-07-02 08:19:26 +0530

    refactor: remove unused notification preferences handling from AccountService

--- src/apps/backend/modules/account/account_service.py
+++ src/apps/backend/modules/account/account_service.py
@@ -1,23 +1,13 @@
-from datetime import datetime
-
-from pymongo import ReturnDocument
-
 from modules.account.internal.account_reader import AccountReader
 from modules.account.internal.account_writer import AccountWriter
-from modules.account.internal.store.account_notification_preferences_model import AccountNotificationPreferencesModel
-from modules.account.internal.store.account_notification_preferences_repository import (
-    AccountNotificationPreferencesRepository,
-)
 from modules.account.types import (
     Account,
     AccountSearchByIdParams,
     AccountSearchParams,
     CreateAccountByPhoneNumberParams,
     CreateAccountByUsernameAndPasswordParams,
-    NotificationPreferences,
     PhoneNumber,
     ResetPasswordParams,
-    UpdateNotificationPreferencesParams,
 )
 from modules.authentication.authentication_service import AuthenticationService
 from modules.authentication.types import CreateOTPParams
@@ -26,11 +16,7 @@ from modules.authentication.types import CreateOTPParams
 class AccountService:
     @staticmethod
     def create_account_by_username_and_password(*, params: CreateAccountByUsernameAndPasswordParams) -> Account:
-        account = AccountWriter.create_account_by_username_and_password(params=params)
-        default_prefs = AccountNotificationPreferencesModel(account_id=account.id, id=None).to_bson()
-        AccountNotificationPreferencesRepository.collection().insert_one(default_prefs)
-
-        return account
+        return AccountWriter.create_account_by_username_and_password(params=params)
 
     @staticmethod
     def get_account_by_phone_number(*, phone_number: PhoneNumber) -> Account:
@@ -42,9 +28,6 @@ class AccountService:
 
         if account is None:
             account = AccountWriter.create_account_by_phone_number(params=params)
-
-            default_prefs = AccountNotificationPreferencesModel(account_id=account.id, id=None).to_bson()
-            AccountNotificationPreferencesRepository.collection().insert_one(default_prefs)
 
         create_otp_params = CreateOTPParams(phone_number=params.phone_number)
         AuthenticationService.create_otp(params=create_otp_params)
@@ -78,43 +61,3 @@ class AccountService:
     @staticmethod
     def get_account_by_username_and_password(*, params: AccountSearchParams) -> Account:
         return AccountReader.get_account_by_username_and_password(params=params)
-
-    @staticmethod
-    def get_notification_preferences(*, account_id: str) -> NotificationPreferences:
-        preferences = AccountNotificationPreferencesRepository.collection().find_one({"account_id": account_id})
-
-        if not preferences:
-            default_prefs = AccountNotificationPreferencesModel(account_id=account_id, id=None).to_bson()
-
-            result = AccountNotificationPreferencesRepository.collection().insert_one(default_prefs)
-            preferences = AccountNotificationPreferencesRepository.collection().find_one({"_id": result.inserted_id})
-
-        return NotificationPreferences(
-            email_enabled=preferences.get("email_enabled", True),
-            sms_enabled=preferences.get("sms_enabled", True),
-            push_enabled=preferences.get("push_enabled", True),
-        )
-
-    @staticmethod
-    def update_notification_preferences(*, params: UpdateNotificationPreferencesParams) -> NotificationPreferences:
-        AccountReader.get_account_by_id(params=AccountSearchByIdParams(id=params.account_id))
-
-        notification_preferences = {
-            "email_enabled": params.email_enabled,
-            "push_enabled": params.push_enabled,
-            "sms_enabled": params.sms_enabled,
-            "updated_at": datetime.now(),
-        }
-
-        result = AccountNotificationPreferencesRepository.collection().find_one_and_update(
-            {"account_id": params.account_id},
-            {"$set": notification_preferences},
-            upsert=True,
-            return_document=ReturnDocument.AFTER,
-        )
-
-        return NotificationPreferences(
-            email_enabled=result.get("email_enabled", True),
-            sms_enabled=result.get("sms_enabled", True),
-            push_enabled=result.get("push_enabled", True),
-        )
