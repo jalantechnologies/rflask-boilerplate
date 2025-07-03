55a16c7ac481194ab376b6603485b9ebdc36581b
Sarthak Gaikwad <sarthak.gaikwad@bettrhq.com>
2025-07-04 00:57:33 +0530

    feat: refactor account routes to use constants for route definitions

--- src/apps/backend/modules/account/rest_api/account_router.py
+++ src/apps/backend/modules/account/rest_api/account_router.py
@@ -1,13 +1,29 @@
+from typing import Final
+
 from flask import Blueprint
 
 from modules.account.rest_api.account_view import AccountView
 
 
 class AccountRouter:
+    ACCOUNTS_ROUTE: Final[str] = "/accounts"
+    ACCOUNT_BY_ID_ROUTE: Final[str] = "/accounts/<id>"
+
     @staticmethod
     def create_route(*, blueprint: Blueprint) -> Blueprint:
-        blueprint.add_url_rule("/accounts", view_func=AccountView.as_view("account_view"))
-        blueprint.add_url_rule("/accounts/<id>", view_func=AccountView.as_view("account_view_by_id"), methods=["GET"])
-        blueprint.add_url_rule("/accounts/<id>", view_func=AccountView.as_view("account_update"), methods=["PATCH"])
-        blueprint.add_url_rule("/accounts/<id>", view_func=AccountView.as_view("account_delete"), methods=["DELETE"])
+
+        blueprint.add_url_rule(
+            rule=AccountRouter.ACCOUNTS_ROUTE,
+            endpoint="create_account",
+            view_func=AccountView.as_view("create_account"),
+            methods=["POST"],
+        )
+
+        blueprint.add_url_rule(
+            rule=AccountRouter.ACCOUNT_BY_ID_ROUTE,
+            endpoint="account_by_id",
+            view_func=AccountView.as_view("account_by_id"),
+            methods=["GET", "PATCH", "DELETE"],
+        )
+
         return blueprint
